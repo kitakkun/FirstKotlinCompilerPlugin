b01a78eb0a0297ce53814e831d4e004b72df57b2
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-09-18 17:21:51 +0900

    implement MyIrGenerationExtension

--- kotlin-plugin/src/main/kotlin/com/example/MyIrGenerationExtension.kt
+++ kotlin-plugin/src/main/kotlin/com/example/MyIrGenerationExtension.kt
@@ -1,12 +1,21 @@
 package com.example
 
 import org.jetbrains.kotlin.backend.common.IrElementTransformerVoidWithContext
+import org.jetbrains.kotlin.backend.common.extensions.FirIncompatiblePluginAPI
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.IrStatement
+import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.declarations.impl.IrVariableImpl
+import org.jetbrains.kotlin.ir.expressions.IrBlockBody
+import org.jetbrains.kotlin.ir.expressions.impl.IrCallImpl
+import org.jetbrains.kotlin.ir.symbols.impl.IrVariableSymbolImpl
+import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 
 class MyIrGenerationExtension(
     private val annotations: List<String>,
@@ -25,7 +34,41 @@ class MyIrTransformer(
     private val annotations: List<String>,
     private val pluginContext: IrPluginContext,
 ) : IrElementTransformerVoidWithContext() {
+    @OptIn(FirIncompatiblePluginAPI::class)
     override fun visitFunctionNew(declaration: IrFunction): IrStatement {
-        TODO("Not implemented yet")
+        val transformed = super.visitFunctionNew(declaration)
+        val hasAnnotation = annotations.any { declaration.hasAnnotation(FqName(it)) }
+        if (!hasAnnotation) return transformed
+        val body = declaration.body as? IrBlockBody ?: return transformed
+
+        val currentTimeMillisFunction = pluginContext.referenceFunctions(FqName("kotlin.system.currentTimeMillis"))
+            .single { it.owner.valueParameters.isEmpty() }
+
+        val currentTimeMillisCall = IrCallImpl(
+            startOffset = declaration.startOffset,
+            endOffset = declaration.endOffset,
+            type = pluginContext.irBuiltIns.longType,
+            symbol = currentTimeMillisFunction,
+            typeArgumentsCount = 0,
+            valueArgumentsCount = 0,
+        )
+
+        val startTimeVariable = IrVariableImpl(
+            startOffset = declaration.startOffset,
+            endOffset = declaration.endOffset,
+            origin = IrDeclarationOrigin.DEFINED,
+            symbol = IrVariableSymbolImpl(),
+            name = Name.identifier("startTime"),
+            isVar = false,
+            isConst = false,
+            isLateinit = false,
+            type = pluginContext.irBuiltIns.longType,
+        )
+
+        startTimeVariable.initializer = currentTimeMillisCall
+
+        body.statements.add(0, startTimeVariable)
+
+        return transformed
     }
 }
