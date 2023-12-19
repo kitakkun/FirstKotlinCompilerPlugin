d3ed0612f0b8674a8584e5091630baacfe825b53
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-12-19 18:40:31 +0900

    remove buildscript and apply plugin via plugins block

--- demo/settings.gradle.kts
+++ demo/settings.gradle.kts
@@ -1,14 +1,17 @@
 pluginManagement {
     repositories {
         mavenCentral()
+        mavenLocal()
     }
     plugins {
         kotlin("jvm") version "1.9.0" apply false
+        id("com.example.my-plugin") version "1.0.0" apply false
     }
 }
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+        mavenLocal()
     }
 }
